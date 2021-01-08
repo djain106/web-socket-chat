7f6a05e032df7bd3f409f7f0108dfcd436327665
Daksh Jain <dakshbjain@gmail.com>
2021-01-07 21:45:49 -0500

    created websocket server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Web Chat
 Application that permits users to connect and participate in a live chat with other clients accessing the same server through the use of websockets.
+
+Built following the guide found [here](https://www.youtube.com/watch?v=LenNpb5zqGE)
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import websocket from 'websocket';
+import websocket, { connection } from 'websocket';
 import http from 'http';
 
 const SOCKET_SERVER_PORT = 8000;
@@ -14,3 +14,32 @@ const wsServer = new websocket.server({
 });
 
 const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+}
+
+wsServer.on('request', function (request) {
+    console.log((new Date()) + ' Received a new connection from origin ' + request.origin + '.');
+
+    var userId = getUniqueID();
+    // Add logic for verifying origin of request.
+    const connection = request.accept(null, request.origin);
+    client[userID] = connection;
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ', message.utf8Data);
+
+            // broadcasting message to all connected clients
+            for (key in clients) {
+                clients[key].sendUTF(message.utf8Data);
+                console.log('Sent Message to: ', clients[key]);
+            }
+        }
+    })
+
+})
